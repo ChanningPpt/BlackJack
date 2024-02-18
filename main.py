b409586289890a9bd5f8f748675c52d591f4159b
Channing <channing.phong@gmail.com>
2024-02-17 23:58:46 -0500

    Update

--- main.py
+++ main.py
@@ -44,38 +44,65 @@ def calculateHandValue(hand):
 playerHand = []
 dealerHand = []
 
-dealCards(deck, playerHand)
-dealCards(deck, playerHand)
-dealCards(deck, dealerHand)
-dealCards(deck, dealerHand)
+# Chip and betting system
+player_chips = 100  # Starting chips
 
 while True:
-    print(f'Player hand: {playerHand} ({calculateHandValue(playerHand)})')
-    print(f'Dealer hand: [{dealerHand[0]}, <face down>]')
+    print(f'You have {player_chips} chips.')
+    bet = int(input('Place your bet: '))
 
-    if calculateHandValue(playerHand) > 21:
-        print('Player busts!')
-        break
-
-    action = input('Do you want to hit or stand? ').lower()
-    if action == 'hit':
-        dealCards(deck, playerHand)
-    elif action == 'stand':
-        break
-    else:
-        print('Invalid action. Please try again.')
+    if bet > player_chips:
+        print("You don't have enough chips!")
         continue
 
-print(f'Player hand: {playerHand} ({calculateHandValue(playerHand)})')
-print(f'Dealer hand: {dealerHand} ({calculateHandValue(dealerHand)})')
+    player_chips -= bet
 
-if calculateHandValue(playerHand) > 21:
-    print('Player busts!')
-elif calculateHandValue(dealerHand) > 21:
-    print('Dealer busts! Player wins!')
-elif calculateHandValue(playerHand) > calculateHandValue(dealerHand):
-    print('Player wins!')
-elif calculateHandValue(playerHand) < calculateHandValue(dealerHand):
-    print('Dealer wins!')
-else:
-    print('Push!')
+    playerHand.clear()
+    dealerHand.clear()
+
+    dealCards(deck, playerHand)
+    dealCards(deck, playerHand)
+    dealCards(deck, dealerHand)
+    dealCards(deck, dealerHand)
+
+    while True:
+        print(f'Player hand: {playerHand} ({calculateHandValue(playerHand)})')
+        print(f'Dealer hand: [{dealerHand[0]}, <face down>]')
+
+        if calculateHandValue(playerHand) > 21:
+            print('Player busts!')
+            break
+
+        action = input('Do you want to hit or stand? ').lower()
+        if action == 'hit':
+            dealCards(deck, playerHand)
+        elif action == 'stand':
+            break
+        else:
+            print('Invalid action. Please try again.')
+            continue
+
+    print(f'Player hand: {playerHand} ({calculateHandValue(playerHand)})')
+    print(f'Dealer hand: {dealerHand} ({calculateHandValue(dealerHand)})')
+
+    if calculateHandValue(playerHand) > 21:
+        print('Dealer wins!')
+    elif calculateHandValue(dealerHand) > 21:
+        print('Player wins!')
+        player_chips += 2 * bet  # Player wins double the bet
+    elif calculateHandValue(playerHand) > calculateHandValue(dealerHand):
+        print('Player wins!')
+        player_chips += 2 * bet  # Player wins double the bet
+    elif calculateHandValue(playerHand) < calculateHandValue(dealerHand):
+        print('Dealer wins!')
+    else:
+        print('Push!')
+        player_chips += bet  # Return the bet to the player in case of a push
+
+    if player_chips == 0:
+        print("You've run out of chips! Game over!")
+        break
+
+    play_again = input('Do you want to play again? (yes/no) ').lower()
+    if play_again != 'yes':
+        break
